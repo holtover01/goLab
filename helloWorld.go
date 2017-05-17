9e247f7a5dca09b57805c4dacb57734bc983c126
Ian Overholt <bryceoverholt@gmail.com>
2017-05-17 08:39:45 -0400

    Add Everything

---
+++ helloWorld.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world")
+    fmt.Println("go" + "lang")
+    //Integers and floats.
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+    //Booleans, with boolean operators as you’d expect.
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+
+    //var declares 1 or more variables.
+    var a string = "initial"
+    fmt.Println(a)
+
+    //You can declare multiple variables at once.
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    //Go will infer the type of initialized variables.
+    var d = true
+    fmt.Println(d)
+
+    //Variables declared without a corresponding initialization are zero-valued. For example, the zero value for an int is 0.
+    var e int
+    fmt.Println(e)
+
+    //The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "short" in this case.
+    f := "short"
+    fmt.Println(f)
+
+    //https://gobyexample.com/constants
+
+    //https://github.com/golang/example
+
+    // https://github.com/golang/go/wiki/Projects
+}
